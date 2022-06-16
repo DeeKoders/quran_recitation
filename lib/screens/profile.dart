85a7e5672a1d65fa266564ddb480a1cd6839ea83
DeeKoders <deekoders@gmail.com>
2022-06-17 02:24:29 +0500

    j

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return (const Center(child: Text("Profile Page")));
+    return (Column(
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 50),
+          height: MediaQuery.of(context).size.height / 4,
+          width: MediaQuery.of(context).size.width / 2.5,
+          child: Image.asset("images/muslim.png"),
+        ),
+      ],
+    ));
   }
 }
